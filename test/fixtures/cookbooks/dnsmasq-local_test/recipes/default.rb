b7e3115f7530560d51608dd0be5577679b8690ef
Jonathan Hartman <j@p4nt5.com>
2016-06-23 12:32:09 -0700

    Only tinker with resolvconf in Docker CI builds

--- test/fixtures/cookbooks/dnsmasq-local_test/recipes/default.rb
+++ test/fixtures/cookbooks/dnsmasq-local_test/recipes/default.rb
@@ -1,14 +1,17 @@
 # Encoding: UTF-8
 
-# Fake out the Docker build and its immutable resolv.conf.
-directory '/var/lib/resolvconf'
-file '/var/lib/resolvconf/linkified'
-
 execute 'apt-get update'
-%w(apt-utils resolvconf).each { |p| package p }
 
-service 'resolvconf' do
-  action [:enable, :start]
+if %w(docker lxc).include?(node['virtualization']['system'])
+  # Fake out the Docker build and its immutable resolv.conf.
+  directory '/var/lib/resolvconf'
+  file '/var/lib/resolvconf/linkified'
+
+  %w(apt-utils resolvconf).each { |p| package p }
+
+  service 'resolvconf' do
+    action [:enable, :start]
+  end
 end
 
 include_recipe 'dnsmasq-local'
