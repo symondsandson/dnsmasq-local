48ca4c44f25e228d8da3f9e4eeda9e485546ccfe
Jonathan Hartman <j@p4nt5.com>
2016-08-02 23:35:04 -0700

    Only install resolvconf on Debian test platforms

--- test/fixtures/cookbooks/dnsmasq-local_test/recipes/default.rb
+++ test/fixtures/cookbooks/dnsmasq-local_test/recipes/default.rb
@@ -3,12 +3,13 @@
 
 execute 'apt-get update' if node['platform_family'] == 'debian'
 
-if %w(docker lxc).include?(node['virtualization']['system'])
+if %w(docker lxc).include?(node['virtualization']['system']) &&
+    node['platform_family'] == 'debian'
   # Fake out the Docker build and its immutable resolv.conf.
   directory '/var/lib/resolvconf'
   file '/var/lib/resolvconf/linkified'
 
-  package 'apt-utils' if node['platform_family'] == 'debian'
+  package 'apt-utils'
   package 'resolvconf'
 
   service 'resolvconf' do
