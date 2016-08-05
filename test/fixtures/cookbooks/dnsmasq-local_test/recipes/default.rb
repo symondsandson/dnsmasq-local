aac9452cba0c9aa68a1406e9a69f257791db70a0
Jonathan Hartman <j@p4nt5.com>
2016-08-05 09:25:26 -0700

    Install NetworkManager in RHEL7 test instances

--- test/fixtures/cookbooks/dnsmasq-local_test/recipes/default.rb
+++ test/fixtures/cookbooks/dnsmasq-local_test/recipes/default.rb
@@ -3,17 +3,25 @@
 
 execute 'apt-get update' if node['platform_family'] == 'debian'
 
-if %w(docker lxc).include?(node['virtualization']['system']) &&
-   node['platform_family'] == 'debian'
-  # Fake out the Docker build and its immutable resolv.conf.
-  directory '/var/lib/resolvconf'
-  file '/var/lib/resolvconf/linkified'
+if %w(docker lxc).include?(node['virtualization']['system'])
+  case node['platform_family']
+  when 'debian'
+    # Fake out the Docker build and its immutable resolv.conf.
+    directory '/var/lib/resolvconf'
+    file '/var/lib/resolvconf/linkified'
+    package 'apt-utils'
+    package 'resolvconf'
 
-  package 'apt-utils'
-  package 'resolvconf'
-
-  service 'resolvconf' do
-    action [:enable, :start]
+    service 'resolvconf' do
+      action [:enable, :start]
+    end
+  when 'rhel'
+    if node['platform_version'].to_i >= 7
+      package 'NetworkManager'
+      service 'NetworkManager' do
+        action [:enable, :start]
+      end
+    end
   end
 end
 
